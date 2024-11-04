b4addff681c0f8dbc8701663dbd7131e94572a23
Alan R <alan.ristic@gmail.com>
2024-11-04 15:56:53 +0100

    Added SCSS package to the project and output test scss to confirm it's working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import "./categories.style.scss"
+
 const App = () => {
-  return <div className="App">Hello World (App.js)</div>
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      description: "Category description goes here.",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      description: "Category description goes here.",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      description: "Category description goes here.",
+    },
+  ]
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ id, title, description }) => (
+        <div className="category-container">
+          <div className="category-image"></div>
+          <div className="category-body">
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <p>Shop Now!</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default App
