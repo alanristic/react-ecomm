960714741948fe746e91f8d3ac48184ae4535e15
Alan R <alan.ristic@gmail.com>
2024-11-04 16:30:14 +0100

    Added fonts, split base App into components

--- src/App.js
+++ src/App.js
@@ -1,38 +1,7 @@
-import "./categories.style.scss"
+import Directory from "./components/directory/directory.component"
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      description: "Category description goes here.",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      description: "Category description goes here.",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      description: "Category description goes here.",
-    },
-  ]
-
-  return (
-    <div className="categories-container">
-      {categories.map(({ id, title, description }) => (
-        <div className="category-container">
-          <div className="category-image"></div>
-          <div className="category-body">
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <p>Shop Now!</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  )
+  return <Directory></Directory>
 }
 
 export default App
