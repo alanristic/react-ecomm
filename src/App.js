45089b979c9b3c6dd81ef501ac1854ba19e47544
Alan R <alan.ristic@gmail.com>
2024-11-05 11:41:47 +0100

    Added React Router comp. and navbar with test links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import Directory from "./components/directory/directory.component"
+import { Routes, Route } from "react-router-dom"
+
+import Navigation from "./routes/navigation/navigation.component"
+import Home from "./routes/home/home.component"
+
+const Shop = () => {
+  return <h1>Shop Page</h1>
+}
 
 const App = () => {
-  return <Directory></Directory>
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<Shop />}></Route>
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
